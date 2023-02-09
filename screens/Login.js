ef191d345db49a504431879504b22dbb16bc59a1
vikash kumar <vkvikashkumar987@gmail.com>
2023-02-09 22:33:11 +0530

    second

---
+++ screens/Login.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Image, SafeAreaView, TouchableOpacity, StatusBar, Alert } from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../config/firebase";
+
+const backImage = require("../assets/backimg.png")
+
+export default function Login({ navigation }) {
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
+
+     const onHandleLogin = () => {
+          if (email !== "" && password !== "") {
+               signInWithEmailAndPassword(auth, email, password)
+                    .then(() => console.log("LoginSucces"))
+                    .catch((err) => Alert.alert("Login Error", err.message));
+          }
+     };
+
+     return (
+          <View style={styles.container}>
+               <Image source={backImage} style={styles.backImage} />
+               <View style={styles.whiteSheet}/>
+          </View>
+     )
+
+}
+
+const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          backgroundColor: "#fff",
+     },
+     title: {
+          fontSize: 36,
+          fontWeight: 'bold',
+          color: "orange",
+          alignSelf: "center",
+          paddingBottom: 24,
+     },
+     input: {
+          backgroundColor: "#F6F7FB",
+          height: 58,
+          marginBottom: 20,
+          fontSize: 16,
+          borderRadius: 10,
+          padding: 12,
+     },
+     backImage: {
+          width: "100%",
+          height: 340,
+          position: "absolute",
+          top: 0,
+          resizeMode: 'cover',
+     },
+     whiteSheet: {
+          width: '100%',
+          height: '75%',
+          position: "absolute",
+          bottom: 0,
+          backgroundColor: '#fff',
+          borderTopLeftRadius: 60,
+     },
+     form: {
+          flex: 1,
+          justifyContent: 'center',
+          marginHorizontal: 30,
+     },
+     button: {
+          backgroundColor: '#f57c00',
+          height: 58,
+          borderRadius: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 40,
+     },
+});
