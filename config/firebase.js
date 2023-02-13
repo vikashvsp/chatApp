bddd8ceae9e7f514743f8e83688b41c7aef99c22
vikash kumar <vkvikashkumar987@gmail.com>
2023-02-13 09:51:18 +0530

    final commit

--- config/firebase.js
+++ config/firebase.js
@@ -5,16 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-     apiKey: Constants.manifest.extra.apiKey,
-     authDomain: Constants.manifest.extra.authDomain,
-     projectId: Constants.manifest.extra.projectId,
-     storageBucket: Constants.manifest.extra.storageBucket,
-     messagingSenderId: Constants.manifest.extra.messagingSenderId,
-     appId: Constants.manifest.extra.appId,
-     databaseURL: Constants.manifest.extra.databaseURL
+     apiKey: "AIzaSyD-VRMDSd6n3lBKND-jgx0rP7vTcs_Ni-A",
+     authDomain: "chatapp-acc33.firebaseapp.com",
+     projectId: "chatapp-acc33",
+     storageBucket: "chatapp-acc33.appspot.com",
+     messagingSenderId: "600635671921",
+     appId: "1:600635671921:web:87d65bfa9032fc998ba2d0"
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-export const auth=getAuth();
+export const auth = getAuth();
 export const database = getFirestore();
---
+++ screens/Home.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { FontAwesome } from '@expo/vector-icons';
+import colors from "../colors";
+import { Entypo } from '@expo/vector-icons';
+const appIcon = "https://www.pngfind.com/pngs/m/161-1616288_chat-app-icon-png-transparent-png.png"
+
+const Home = () => {
+     const navigation = useNavigation();
+     useEffect(() => {
+          navigation.setOptions({
+               headerLeft: () => (
+                    <FontAwesome name="search" size={24} color={colors.gray} style={{ marginLeft: 15 }} />
+               ),
+               headerRight: () => (
+                    <Image
+                         source={{ uri: appIcon }}
+                         style={{
+                              width: 40,
+                              height: 40,
+                              marginRight: 15,
+                         }}
+                    />
+               ),
+          });
+     }, [navigation]);
+
+     return (
+          <View style={styles.container}>
+               <TouchableOpacity
+                    onPress={() => navigation.navigate("Chat")}
+                    style={styles.chatButton}
+               >
+                    <Entypo name="chat" size={24} color={colors.lightGray} />
+               </TouchableOpacity>
+          </View>
+     );
+
+};
+
+export default Home;
+const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end',
+          backgroundColor: "#fff",
+     },
+     chatButton: {
+          backgroundColor: colors.primary,
+          height: 50,
+          width: 50,
+          borderRadius: 25,
+          alignItems: 'center',
+          justifyContent: 'center',
+          shadowColor: colors.primary,
+          shadowOffset: {
+               width: 0,
+               height: 2,
+          },
+          shadowOpacity: .9,
+          shadowRadius: 8,
+          marginRight: 20,
+          marginBottom: 50,
+     }
+});
